ef27c6ab53e892677b49ee189392a91f3d512b4a
Sasha <skv43r@gmail.com>
2024-10-25 17:44:19 +0300

    refactor: added Jinja2Templates, updated file paths, removed uvicorn and requests imports, and modified main_page function to use templates and read from data.json

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import aiohttp
 import json
 import os
 import aiofiles
-import requests
-import uvicorn
 
 app = FastAPI()
 
@@ -16,13 +15,15 @@ if not os.path.exists("backend/public"):
 app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 app.mount("/public", StaticFiles(directory="backend/public"), name="public")
 
+templates = Jinja2Templates(directory="backend/static")
+
 @app.get("/external-data")
 async def get_external_data():
     async with aiohttp.ClientSession() as session:
         try:
             async with session.get("https://api.npoint.io/88fcfcbf4fde970ba6f2") as response:
                 data = await response.json()
-                async with aiofiles.open ("data.json", "w") as file:
+                async with aiofiles.open ("backend/data.json", "w") as file:
                     await file.write(json.dumps(data, indent=4))
 
 
@@ -31,7 +32,7 @@ async def get_external_data():
                     user_id = user["id"]
 
                     file_name = f"user_{user_id}.jpg"
-                    file_path = os.path.join("public", file_name)
+                    file_path = os.path.join("backend/public", file_name)
 
                     async with session.get(avatar_url) as img_response:
                         if img_response.status == 200:
@@ -47,33 +48,23 @@ async def get_external_data():
             return {"error": str(e)}
 
 @app.get("/", response_class=HTMLResponse)
-async def main_page():
-    async with aiofiles.open("backend/static/index.html", "r") as file:
-        html_content = await file.read()
-    return HTMLResponse(content=html_content)
+async def main_page(request: Request):
+    try:
+        async with aiofiles.open("backend/data.json", "r") as file:
+                data = json.loads(await file.read())
+                ids = [user["id"] for user in data["users"]]
+                names = [user["name"] for user in data["users"]]
+                emails = [user["email"] for user in data["users"]]
 
-if  __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
-# @app.get("/test")
-# def get_data():
-#     try:
-#         response = requests.get("https://api.npoint.io/88fcfcbf4fde970ba6f2")
-#         data = response.json()
-#         with open ("data.json", "w") as file:
-#             json.dump(data, file, indent=4)
-
-#         for user in data["users"]:
-#             avatar_link = user["avatar"]
-#             user_id = user["id"]
-
-#             file_name = f"user_{user_id}.jpg"
-#             file_path = os.path.join("public", file_name)
-            
-#             requests.get(avatar_link)
-#             with open(file_path, "wb") as img:
-#                 img.write(requests.get(avatar_link).content)
-
-#         return {"message": "JSON and images saved to public folder"}
-#     except Exception as e:
-#         return {"error": str(e)}
+                
+                return templates.TemplateResponse("index.html", {
+                    "request": request,
+                    "ids": ids,
+                    "names": names,
+                    "emails": emails
+                })
+    except Exception as e:
+        return HTMLResponse(content=f"Error: {str(e)}")
+    
+# if  __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
