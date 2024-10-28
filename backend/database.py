189c5dfa31368bd7f659adfb325090401da788d7
Alexandr <skv43r@gmail.com>
2024-10-28 23:56:41 +0300

    feat(database): implement database configuration

---
+++ backend/database.py
@@ -0,0 +1,16 @@
+from typing import Generator
+from sqlmodel import SQLModel, Session, create_engine
+
+sqlite_file_name = "users.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread" : False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
+
