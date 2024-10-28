41f6578ccfda4bd10f3ba33d8517919da4e4c37d
Alexandr <skv43r@gmail.com>
2024-10-28 23:54:37 +0300

    feat(models): create User model for database

---
+++ backend/models.py
@@ -0,0 +1,8 @@
+from sqlmodel import SQLModel, Field
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    email: str = Field(index=True, unique=True)
+    avatar: str | None = Field(default=None)
