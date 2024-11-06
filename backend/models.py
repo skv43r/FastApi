bea1cc80415ff8081961fd2b37dcc1a90a148e1f
Alexandr <skv43r@gmail.com>
2024-11-06 13:37:59 +0300

    feat(models): add authuser model

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,6 @@
 from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime
 
 
 class User(SQLModel, table=True):
@@ -6,3 +8,11 @@ class User(SQLModel, table=True):
     name: str = Field(index=True)
     email: str = Field(index=True, unique=True)
     avatar: str | None = Field(default=None)
+
+class AuthUser(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True)
+    email: str = Field(index=True, unique=True)
+    password: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    last_login: Optional[datetime] = Field(default=None)
