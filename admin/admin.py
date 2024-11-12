3a1469bdda665ae7df638601ce18b8bdcd38d90b
Alexandr <skv43r@gmail.com>
2024-11-12 21:35:01 +0300

    feat(admin): add the ability to create and save data in the database

--- admin/admin.py
+++ admin/admin.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlmodel import create_engine
+from sqlalchemy import create_engine
 from models import User
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.secret_key = "supersecret"
@@ -21,10 +23,17 @@ class UserAdminView(ModelView):
     column_list = ('id', 'name', 'email', 'avatar')
     form_columns = ('name', 'email', 'avatar')
 
-
-
+    form_extra_fields = {
+        'name': StringField('User  Name', validators=[DataRequired()]),
+        'email': StringField('Email Address', validators=[DataRequired(), Email()]),
+        'avatar': StringField('Avatar URL'),
+    }
 
 admin.add_view(UserAdminView(User, SessionLocal))
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    SessionLocal.remove()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
