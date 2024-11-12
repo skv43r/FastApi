33d8f63475752865953803d1b2149962d1291900
Alexandr <skv43r@gmail.com>
2024-11-12 19:34:23 +0300

    refactor(admin): edit links to compose

--- admin/admin.py
+++ admin/admin.py
@@ -3,11 +3,11 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlmodel import create_engine
-from backend.models import User
+from models import User
 
 app = Flask(__name__)
 app.secret_key = "supersecret"
-engine = create_engine("postgresql://postgres:Worldof123@localhost:5432/Users")
+engine = create_engine("postgresql://postgres:Worldof123@postgres:5432/Users")
 SessionLocal = scoped_session(sessionmaker(bind=engine))
 
 admin = Admin(app, name="Administrator", template_mode="bootstrap3")
@@ -20,11 +20,11 @@ class UserAdminView(ModelView):
 
     column_list = ('id', 'name', 'email', 'avatar')
     form_columns = ('name', 'email', 'avatar')
-    
+
 
 
 
 admin.add_view(UserAdminView(User, SessionLocal))
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
