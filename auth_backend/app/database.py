7742039b936458e178d125950e813be7a8dda10c
Alexandr <skv43r@gmail.com>
2024-11-14 01:04:29 +0300

    refactor(database): move from backend

---
+++ auth_backend/app/database.py
@@ -0,0 +1,12 @@
+from typing import Generator
+from sqlmodel import SQLModel, Session, create_engine
+
+DATABASE_URL = f"postgresql://postgres:Worldof123@postgres:5432/authusers"
+engine = create_engine(DATABASE_URL, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
