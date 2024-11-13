ed516d2ddd039fd1c36592be1629cab14ae4c8ec
Alexandr <skv43r@gmail.com>
2024-11-13 23:54:39 +0300

    refactor(models): remove AuthUser and TelegramOTP

--- backend/models.py
+++ backend/models.py
@@ -9,16 +9,16 @@ class User(SQLModel, table=True):
     email: str = Field(index=True, unique=True)
     avatar: str | None = Field(default=None)
 
-class AuthUser(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    username: str = Field(index=True, unique=True)
-    email: str = Field(index=True, unique=True)
-    password: str
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    last_login: Optional[datetime] = Field(default=None)
+# class AuthUser(SQLModel, table=True):
+#     id: int | None = Field(default=None, primary_key=True)
+#     username: str = Field(index=True, unique=True)
+#     email: str = Field(index=True, unique=True)
+#     password: str
+#     created_at: datetime = Field(default_factory=datetime.utcnow)
+#     last_login: Optional[datetime] = Field(default=None)
 
-class TelegramOTP(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    username: str = Field(index=True, unique=True)
-    telegram_id: int = Field(..., description="Telegram user ID")
-    otp: str | None = Field(default=None, description="One-time password")
+# class TelegramOTP(SQLModel, table=True):
+#     id: int | None = Field(default=None, primary_key=True)
+#     username: str = Field(index=True, unique=True)
+#     telegram_id: int = Field(..., description="Telegram user ID")
+#     otp: str | None = Field(default=None, description="One-time password")
