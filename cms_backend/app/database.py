11bc3472c3ae987b4ac2f5c8866f4c2736eb6501
Alexandr <skv43r@gmail.com>
2024-11-15 18:27:59 +0300

    refactor(database): edit link

--- cms_backend/app/database.py
+++ cms_backend/app/database.py
@@ -1,7 +1,7 @@
 from typing import Generator
 from sqlmodel import SQLModel, Session, create_engine
 
-DATABASE_URL = f"postgresql://postgres:Worldof123@postgres:5432/cms"
+DATABASE_URL = f"postgresql://postgres:Worldof123@postgres:5432/Users"
 engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
