1f66bd43563213b5674c6f5d472677bae0788a39
Alexandr <skv43r@gmail.com>
2024-11-28 18:32:05 +0300

    feat(routes): add new endpoints

--- application/backend/routes.py
+++ application/backend/routes.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
-from sqlmodel import Session
+from fastapi import APIRouter, Depends, Query
+from sqlmodel import Session, select
+from sqlalchemy import extract
 from database import db
 from models import User
 from user_controller import UserController
 from typing import Annotated
+from models import Service, Trainer, TimeSlot
+from datetime import datetime
 
 SessionDep = Annotated[Session, Depends(db.get_session)]
 router = APIRouter()
@@ -27,3 +30,25 @@ async def edit_user_endpoint(session: SessionDep, user_id: int, user_data: User)
 async def return_user_endpoint(session: SessionDep):
     user_controller = UserController(session)
     return await user_controller.return_users()
+
+@router.get("/api/services")
+async def return_services_endpoint(session: SessionDep):
+    services = session.exec(select(Service)).all()
+    return services
+
+@router.get("/api/trainers")
+async def return_trainers_endpoint(session: SessionDep):
+    trainers = session.exec(select(Trainer)).all()
+    return trainers
+
+@router.get("/api/timeslots")
+async def return_timeslots_endpoint(session: SessionDep, service_id: int, trainer_id: int = Query(..., alias="trainerId"), date: str = Query(..., format="date")):
+    timeslots = session.exec(
+        select(TimeSlot).where(
+            TimeSlot.trainer_id == trainer_id,
+            TimeSlot.dates == date,
+            TimeSlot.service_id == service_id
+        )
+    ).all()
+    return timeslots
+
