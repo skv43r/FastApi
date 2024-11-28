a8b22313a31bc0fcac81fd25fd1e2cd720195cdc
Alexandr <skv43r@gmail.com>
2024-11-28 18:30:31 +0300

    fix(config): edit link

--- application/backend/config.py
+++ application/backend/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:Worldof123@postgres:5432/Users"
+    DATABASE_URL: str = "postgresql://postgres:Worldof123@localhost:5432/Users"
     ECHO_SQL: bool = True
 
     class Config:
