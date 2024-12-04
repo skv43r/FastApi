882abc5b145d328677e76e356d547ee2b101c203
Alexandr <skv43r@gmail.com>
2024-12-04 12:14:34 +0300

    chore: remove files

--- auth_backend/app/bot/handlers.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Dispatcher, types
-from aiogram.filters import Command
-
-def register_handlers(dp: Dispatcher):
-    @dp.message(Command("start"))
-    async def cmd_start(message: types.Message):
-        await message.answer("Hello! I'm your registration bot.")
