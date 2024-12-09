e1aa89e10504aea76fdd564c27b7df0d47a1b05b
Sasha <skv43r@gmail.com>
2024-12-09 17:54:08 +0300

    feat(main): create FastAPI app

---
+++ admin/backend/main.py
@@ -0,0 +1,25 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from routes import router as rest_router
+from database import db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db.create_db_and_tables()
+    yield 
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(rest_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if  __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
