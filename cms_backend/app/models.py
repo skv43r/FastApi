9274a9dbed4d46fe57317114159bf1b901839f95
Sasha <skv43r@gmail.com>
2025-01-05 17:01:46 +0300

    chore(cms_backend): delete service

--- cms_backend/app/models.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    email: str = Field(index=True, unique=True)
-    avatar: str | None = Field(default=None)
