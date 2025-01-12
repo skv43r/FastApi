78a07d31ae042be50ef80ee594712d478e5f41e6
Alexandr <skv43r@gmail.com>
2025-01-12 14:32:24 +0300

    refactor(config): edit db url

--- auth/backend/app/config.py
+++ auth/backend/app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:Worldof123@postgres:5432/authusers"
+    AUTH_BACKEND_DB_URL: str
     ECHO_SQL: bool = True
 
     class Config:
